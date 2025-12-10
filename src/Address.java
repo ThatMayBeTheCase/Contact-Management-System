64d92994d0eec0aecaeb826dd0642442d02a1e44
Timmy W <137299906+ThatMayBeTheCase@users.noreply.github.com>
2025-12-10 12:29:28 +0100

    Feature/scaffold (#1)
    * Scaffold.
    
    * Scaffold.
    
    * Remove legacy java folder
    
    * Delete .gitignore

---
+++ src/Address.java
@@ -0,0 +1,3 @@
+public class Address {
+    // TODO: fields: streetName, streetNumber, city, postalCode
+}
---
+++ src/ConsoleUI.java
@@ -0,0 +1,11 @@
+public class ConsoleUI {
+    private final ContactManager manager;
+
+    public ConsoleUI(ContactManager manager) {
+        this.manager = manager;
+    }
+
+    public void run() {
+        // TODO: implement console loop, parse commands, call manager
+    }
+}
