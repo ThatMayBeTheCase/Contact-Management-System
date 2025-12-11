366a4fae6c0de4c4bd31ade5d259a3d95f38e08c
MuamerBrankovic <ako.brankovic@outlook.com>
2025-12-11 15:15:30 +0100

    Feature/contact class (#5)
    * Added CRUD methods in ContactManager
    
    * feat: add Contact and ContactManager classes with CRUD operations

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactManager {
     // TODO: hold contacts (e.g., List<Contact>) and implement CRUD + search
+    private List<Contact> contacts = new ArrayList<>();
+
+    public void addContact(Contact contact){
+        contacts.add(contact);
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void updateContact(Contact contact, String newName, int newAge, String newAddress, String newPhone) {
+        contact.setName(newName);
+        contact.setAge(newAge);
+        contact.setAddress(newAddress);
+        contact.setPhone(newPhone);
+    }
+
+    public void deleteContact(Contact contact){
+        contacts.remove(contact);
+    }
+
 }
