21f1600e8c0528ed8d2ecc46eb69a735b96b2bcc
Moody Amberdal <129885358+moodyambr@users.noreply.github.com>
2025-12-11 12:06:13 +0100

    Complete Address class implementation for Story19 (#4)

--- src/Address.java
+++ src/Address.java
@@ -1,3 +1,25 @@
 public class Address {
     // TODO: fields: streetName, streetNumber, city, postalCode
-}
+
+        // Fields of the Address class
+        private String city;
+        private String postalCode;
+        private String streetName;
+        private String portNumber;
+
+        // Constructor of the Address class
+        public Address(String city, String postalCode, String streetName, String portNumber) {
+            this.city = city;
+            this.postalCode = postalCode;
+            this.streetName = streetName;
+            this.portNumber = portNumber;
+
+            //
+        }
+        @Override
+        public String toString() {
+            return streetName + " " + portNumber + ", " + postalCode + " " + city;
+        }
+
+    }
+
