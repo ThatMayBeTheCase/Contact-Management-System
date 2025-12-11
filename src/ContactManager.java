da11faae2f4165e21f5807696688549f73c6723a
Muamer Brankovic <ako.brankovic@outlook.com>
2025-12-11 11:00:57 +0100

    Added CRUD methods in ContactManager

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactManager {
     // TODO: hold contacts (e.g., List<Contact>) and implement CRUD + search
+    private List<Contact> contacts = new ArrayList<>();
+
+    public void addContact(Contact contact){
+        contacts.add(contact);
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    //public void updateContact(String name, int age, String address, int phone){
+   // }
+
+    public void deleteContact(Contact contact){
+        contacts.remove(contact);
+    }
 }
